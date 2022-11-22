bdae3e2dacbd072dd615ee843d71c86fab923b66
Gitux1602 <gdelmercado@outlook.com>
2022-11-22 14:55:26 -0600

    Igual

--- src/app/pages/list-products/list-products.component.ts
+++ src/app/pages/list-products/list-products.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { ThisReceiver } from '@angular/compiler';
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Product } from 'src/app/interfaces/product';
@@ -12,19 +12,25 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent implements OnInit {
-  
+
   listProducts: any = []
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this.getListProducts();
+
   }
-  getListProducts(){
-     this._productService.getListProducts().subscribe((data)=>{
+  getListProducts() {
+    this._productService.getListProducts().subscribe((data: Product[]) => {
       this.listProducts = data
       console.log(this.listProducts);
     })
-
+  }
+  deleteProducts(id: number) {
+    this._productService.deleteProducts(id).subscribe(() => {
+      this.getListProducts();
+      console.log(id);
+    })
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,12 +9,26 @@ import { Product } from '../interfaces/product';
     providedIn: 'root'
   })
   export class ProductService{ 
-    
-  constructor(private http: HttpClient ){}
+    private myAppUrl: string;
+  private myApiUrl: string;
+  constructor(private http: HttpClient ){
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = 'api/list/'
+  }
 
     getListProducts(): Observable<Product[]> {
       //Aqui estaba el error, se usó el url equivocado
       const url = environment.endpoint + 'api/list';
-      return this.http.get<Product[]>(url,{withCredentials: true});
+      return this.http.get<Product[]>(`${this.myAppUrl}${this.myApiUrl}`,{withCredentials: true});
     }
+    
+    deleteProducts(id: number):Observable<void> {
+      //const url = environment.endpoint + 'api/list';
+      return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}${id}`,{withCredentials: true});
+    }
+
+    saveProduct(product: Product):Observable<void>{
+     return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}`,product)
+    }
+
   }
